460435829bff9e34a5c2ccebfefa51aabaeec5a2
Marcos Scheffer <marcos.scheffer.arg@gmail.com>
2023-11-16 16:13:38 -0300

    modificacion a modulos

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -4,12 +4,18 @@ class BandaHoraria(models.Model):
     nombre = models.CharField(max_length=255)
     horario_inicio = models.DateTimeField()
     horario_fin = models.DateTimeField()
+    
+    def __str__(self):
+        return self.nombre
 
 class Curso(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.CharField(max_length=255)
     banda_horaria = models.ForeignKey(BandaHoraria, on_delete = models.CASCADE)
     nota = models.IntegerField()   
+    
+    def __str__(self):
+        return self.nombre
 
 class Alumno(models.Model):
     nombre = models.CharField(max_length=255)
@@ -18,4 +24,7 @@ class Alumno(models.Model):
     telefono = models.CharField(max_length=15)
     correo_electronico = models.EmailField()
     curso = models.ForeignKey(Curso, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
     
